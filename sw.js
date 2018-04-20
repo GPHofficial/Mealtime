772475ed052555b564b9b7f6f449cc3d6d4c3cf7
Puay Hiang Goh <GPHofficial@users.noreply.github.com>
2018-04-20 19:44:14 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = 'cache-v2';
+var CACHE_NAME = 'cache-v2-fix';
 var urlsToPrefetch = [
 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css',
 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css',
@@ -14,16 +14,13 @@ self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(function(cache) {
-        console.log('Opened cache');
-        cache.addAll(urlsToPrefetch.map(function(urlToPrefetch) {
-           return new Request(urlToPrefetch, { mode: 'no-cors' });
-        })).then(function() {
-          console.log('All resources have been fetched and cached.');
+        cache.addAll(urlsToPrefetch).then(function(res){
+          console.log('All resources have been fetched and cached. (install)');
         });
       })
   );
 });
-/*
+
 // https://developers.google.com/web/fundamentals/instant-and-offline/offline-cookbook/
 // Stale-while-revalidate strategy
 var fetchRequest = null;
@@ -52,45 +49,3 @@ self.addEventListener('fetch', function(event) {
     })
   );
 });
-/*
-/*
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        // Cache hit - return response
-        if (response) {
-          return response;
-        }
-
-        // IMPORTANT: Clone the request. A request is a stream and
-        // can only be consumed once. Since we are consuming this
-        // once by cache and once by the browser for fetch, we need
-        // to clone the response.
-        var fetchRequest = event.request.clone();
-
-        return fetch(fetchRequest).then(
-          function(response) {
-            // Check if we received a valid response
-            if(!response || response.status !== 200 || response.type !== 'basic') {
-              return response;
-            }
-
-            // IMPORTANT: Clone the response. A response is a stream
-            // and because we want the browser to consume the response
-            // as well as the cache consuming the response, we need
-            // to clone it so we have two streams.
-            var responseToCache = response.clone();
-
-            caches.open(CACHE_NAME)
-              .then(function(cache) {
-                cache.put(event.request, responseToCache);
-              });
-
-            return response;
-          }
-        );
-      })
-    );
-});
-*/
